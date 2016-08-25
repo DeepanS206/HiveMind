ec8320c4f462686882c3015149a386f2a2ef81d1
Deepan Saravanan <deepan.saravanan83@gmail.com>
2016-08-25 19:21:58 -0400

    removed credentials

--- dbconfig.py
+++ dbconfig.py
@@ -3,5 +3,5 @@ from pymongo import MongoClient
 def connect():
   connection = MongoClient("ds161495.mlab.com", 61495)
   handle = connection["hivemind_db"]
-  handle.authenticate("deepansaravanan", "demouser1")
+  handle.authenticate("deepansaravanan", "********")
   return handle
